08c457474d792961a4c0d6e42afaf9c066591d8f
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-06-15 22:16:52 +0200

    remove shit from monitor

--- src/Monitor.cc
+++ src/Monitor.cc
@@ -11,7 +11,7 @@ Monitor* Monitor::instance = nullptr;
 std::vector<Engine*> Monitor::engines;
 
 void Monitor::handleEvent(Object* sender, const std::string& info) {
-  std::stringstream ss;
+  //std::stringstream ss;
   uint64_t cur_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
   uint64_t total_exec_sec = 0;
   uint64_t total_execs = 0;
@@ -20,10 +20,10 @@ void Monitor::handleEvent(Object* sender, const std::string& info) {
     uint64_t exec_sec = diff ? e->getExecs() * 1000 / diff : 0;
     total_exec_sec += exec_sec;
     total_execs += e->getExecs();
-    ss << e->toString() << " < execs = " << e->getExecs() << " exec/sec = " << exec_sec << " > ";
+    //ss << e->toString() << " < execs = " << e->getExecs() << " exec/sec = " << exec_sec << " > ";
   }
-  Logger::logLine(sender->toString(), " ", info, ": Total execs = ", total_execs, " Total exec/sec = ", total_exec_sec, ", ", ss.str());
-  //Logger::logLine(sender->toString(), " ", info, ": Total execs = ", total_execs, " Total exec/sec = ", total_exec_sec);
+  //Logger::logLine(sender->toString(), " ", info, ": Total execs = ", total_execs, " Total exec/sec = ", total_exec_sec, ", ", ss.str());
+  Logger::logLine(sender->toString(), " ", info, ": Total execs = ", total_execs, " Total exec/sec = ", total_exec_sec);
 }
 
 }
